58bf9f4bda8946bb12e81e9a51e41c1d7cbb388f
RomaKun <roman@gophotoweb.ru>
2024-02-27 12:09:23 +0100

    Fix git error with symbol '

--- homework/raman_kuntsevich/homework_5/task_3.py
+++ homework/raman_kuntsevich/homework_5/task_3.py
@@ -4,6 +4,6 @@ subjects = ['math', 'biology', 'geography']
 
 print(text_template.format(', '.join(students), ', '.join(subjects)))
 
-text_template2 = f'Students {', '.join(students)} study these subjects: {', '.join(subjects)}'
+text_template2 = f"Students {', '.join(students)} study these subjects: {', '.join(subjects)}"
 
 print(text_template2)
