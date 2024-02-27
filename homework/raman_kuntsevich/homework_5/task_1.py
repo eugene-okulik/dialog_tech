10823f0a9a16fc30b96d535dcc2ff3cd3a17cc89
RomaKun <roman@gophotoweb.ru>
2024-02-27 12:00:14 +0100

    Homework_5 done

--- homework/raman_kuntsevich/homework_5/task_1.py
+++ homework/raman_kuntsevich/homework_5/task_1.py
@@ -1,2 +1,8 @@
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
+
+print(name)
+print(last_name)
+print(city)
+print(phone)
+print(country)
