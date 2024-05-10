6da3a32ca914876caaf61f7ec7dbb64dabeae087
RomaKun <roman@gophotoweb.ru>
2024-05-10 17:54:38 +0200

    Homework_28 completed

--- homework/raman_kuntsevich/homework_28/test_task.py
+++ homework/raman_kuntsevich/homework_28/test_task.py
@@ -1,14 +1,15 @@
 from playwright.sync_api import Page, expect, Route
-from time import sleep
 import re
 import json
 
 
 def test_apple_page(page: Page):
+    expected_title = 'яблокофон 15 про'
+
     def change_response(route: Route):
         response = route.fetch()
         body = response.json()
-        body["body"]["digitalMat"][0]["familyTypes"][0]["productName"] = "яблокофон 15 про"
+        body["body"]["digitalMat"][0]["familyTypes"][0]["productName"] = expected_title
         body = json.dumps(body)
         route.fulfill(
             response=response,
@@ -22,5 +23,4 @@ def test_apple_page(page: Page):
     button = page.get_by_role('button', name='Take a closer look').locator('nth=0')
     button.click()
     pop_up_header = page.locator('#rf-digitalmat-overlay-label-0').locator('nth=0')
-    expect(pop_up_header).to_have_text('яблокофон 15 про')
-    sleep(5)
+    expect(pop_up_header).to_have_text(expected_title)
